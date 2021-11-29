717aa60ba385558d8ef9918cd529512c91392568
GilangRizkiBahtiar12 <iyangtogu@gmail.com>
2021-11-29 18:13:20 +0700

    menambahkan file pyhton

---
+++ list1.py
@@ -0,0 +1,16 @@
+list=[1,2,3,4,5,]
+
+
+print("Element ke 3 adalah:", list[2])
+print("ambil Element ke 2 sampai 4 adalah:", list[1:4])
+print("elemen terakhir:", list[5-1])
+
+
+
+list[3] = 6
+
+print("merubah elemen ke 4 dengan nilai yang lain:", list)
+
+
+list[3:] = 6,7
+print("merubah elemen ke 4 sampai elemen yang terakhir",list)
